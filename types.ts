61e7d58c794e584a4910d60d946cca149e3417bd
binarawan <infobisnisbina@gmail.com>
2026-01-31 20:47:35 +0700

    Extract landing page files from zip; remove zip

---
+++ types.ts
@@ -0,0 +1,16 @@
+export interface Testimonial {
+  name: string;
+  city: string;
+  comment: string;
+  stars: number;
+}
+
+export interface BenefitItem {
+  icon: string;
+  title: string;
+}
+
+export interface PackageType {
+  name: string;
+  icon: string;
+}
